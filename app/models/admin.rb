c47c6343a098a5f806a8662bf1ee2262406a89cc
Anthony LLewellyn <drallewellyn@me.com>
2015-09-04 16:45:12 +1000

    migrate db for devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,3 @@
 class Admin < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :omniauthable, :registerable, :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable        
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable  
 end
