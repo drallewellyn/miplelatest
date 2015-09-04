dddab3c2a0f1f2d859b93bd65249f9635f555426
Anthony Llewellyn <drallewellyn@me.com>
2015-09-04 13:16:05 +1000

    set up fontface, add scss for fontface, comment out some theme inclusions in head

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Websitestyle
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.assets.paths << Rails.root.join('vendor', 'app', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
