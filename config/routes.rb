adbfa241a5bb4119170d6fb55f42b8bd954e17c2
Anthony LLewellyn <drallewellyn@me.com>
2015-09-05 14:56:33 +1000

    add post and portfolio controllers

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :posts
+  resources :portfolios
   # Example resource route with options:
   #   resources :products do
   #     member do
