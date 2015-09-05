21f85713f2cdc6c0b2f46249b0ca8d7b15045f29
Anthony LLewellyn <drallewellyn@me.com>
2015-09-05 17:00:46 +1000

    add posts#new view add post controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904064346) do
+ActiveRecord::Schema.define(version: 20150905063536) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150904064346) do
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
