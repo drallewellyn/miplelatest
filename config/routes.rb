c4e498b2c476d15e93ac1d6a2a058792e8ef5b69
Anthony LLewellyn <drallewellyn@me.com>
2015-09-12 11:03:55 +1000

    change admin model to user model plus admin attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
+  devise_for :users, :skip => :registrations
   get 'pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
