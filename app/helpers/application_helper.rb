acad6436678677229f5f3356809da8a8e448594f
Anthony Llewellyn <drallewellyn@me.com>
2015-09-18 21:35:23 +1000

    set up slideoutpanel for admin login via devise, user sign up, set up header for social media

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
     end
     doc.to_html.html_safe
   end
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :skip => :registrations
+  devise_for :users
   get 'pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
