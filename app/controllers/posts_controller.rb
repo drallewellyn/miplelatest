7e43c184cb74da7b790f85d78fac9562b1a035a0
Anthony LLewellyn <drallewellyn@me.com>
2015-09-28 13:45:05 +1000

    railsify hover on blog summary

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
 
   def index
     if params[:tag].present? 
-      @posts = Post.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 10)
+      @posts = Post.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 4)
     else
-      @posts = Post.all.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+      @posts = Post.all.order('created_at DESC').paginate(page: params[:page], per_page: 4)
     end
   end
 
